9d65909a1bee5fc7439aece7728736cf8a75ddc7
erclasergame <erclasergame@gmail.com>
2025-08-14 20:44:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   env: {
     OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
   },
